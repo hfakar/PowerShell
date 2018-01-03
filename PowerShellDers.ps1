5d57590eae23a6ae345840f2a2751b46c6f0b96c
hfakar <hfakar@gmail.com>
2018-01-03 08:44:48 -0800

    continue training

--- PowerShellDers.ps1
+++ PowerShellDers.ps1
@@ -87,3 +87,124 @@ Get-Service | ? {$PSItem.Status -eq "Running"} # Where is alias to where-object
 Get-Service | Where {$_.Name -like "n*" -and $_.Status -eq "Running"}
 
 Get-Process | Where {$_.Name -like "a*"}
+
+1..5
+
+1..5 | foreach {Get-Random -Maximum 100}
+
+1..5 | % {Get-Random -Maximum 100}
+
+Get-Service net* | % {$_.Name + " service is " + $_.Status}
+
+Get-Service Spooler, MpsSvc | Stop-Service
+
+Get-Service Spooler, MpsSvc | Start-Service
+
+"Spooler", "Mpssvc" Stop-Service # Wrong
+
+Get-Process *calc*,notepad | Stop-Process
+
+Get-Help Stop-Process -Full
+
+
+
+#by value vs by property
+
+Get-service MpsSvc, Spooler | select Name
+
+Get-service MpsSvc, Spooler | select Name -ExpandProperty name
+
+Get-service MpsSvc, Spooler | select Name -ExpandProperty name | Stop-Service
+
+Get-service MpsSvc, Spooler | select Name -ExpandProperty name | Start-Service
+
+
+
+#Variables
+$name = "Hfakar"
+
+$name  #hfakar
+
+Get-Variable
+
+$PSVersionTable
+
+#getting input
+
+$name = Read-Host "What is Your name?"
+
+$surname = Read-Host "What is Your surname?"
+
+$fullname = $name + " " + $surname
+
+Write-Host "Your name is $fullname"
+
+[int]$number1 = Read-Host "Enter the first number"
+
+[int]$number2 = Read-Host "Enter the second number"
+
+$result = $number1 + $number2
+
+Write-Host $result
+
+
+###if else conditions
+
+[int]$number=Read-Host "Enter a number"
+if($number -lt 100){
+    Write-Host "$number is less than 100"
+
+
+}
+
+elseif($number -eq 100){
+    Write-host "$number is equal to 100"
+
+}
+
+else{
+    Write-host "$number is greater than 100"
+
+}
+
+#error handling
+
+try{
+    [int]$number=Read-Host "Enter a number"
+if($number -lt 100){
+    Write-Host "$number is less than 100"
+
+
+}
+
+elseif($number -eq 100){
+    Write-host "$number is equal to 100"
+
+}
+
+else{
+    Write-host "$number is greater than 100"
+
+}
+
+}
+catch{
+    Write-Host  "Error" -BackgroundColor Red
+}
+finally{
+    Write-Host "Have a good day"
+}
+
+
+###Functions
+
+function Sum {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true, HelpMessage="Enter the first number")][int]$number1,
+        [Parameter(Mandatory=$true, HelpMessage="Enter the second number")][int]$number2
+    )
+    $result = $number1 + $number2
+    return $result
+}
+Sum
